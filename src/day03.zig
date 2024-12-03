a02f86c5777b685bb5f9102e04738497eb5c7cfe
Denizhan Erdem <derdem@microsoft.com>
2024-12-03 13:46:05 +0000

    day3

---
+++ src/day03.zig
@@ -0,0 +1,38 @@
+const std = @import("std");
+const mvzr = @import("mvzr");
+
+const input = std.mem.trim(u8, @embedFile("inputs/day03.txt"), "\n");
+
+fn doThing(enablable: bool) !u32 {
+    const validOpRegex = mvzr.compile("mul\\(\\d+,\\d+\\)|do\\(\\)|don't\\(\\)").?;
+    var validOpIter = validOpRegex.iterator(input);
+    var acc: u32 = 0;
+    var opEnabled: bool = true;
+    while (validOpIter.next()) |match| {
+        if (match.slice.len == 4) {
+            opEnabled = true;
+        } else if (match.slice.len == 7) {
+            opEnabled = false;
+        } else if (enablable or opEnabled) {
+            const numRegex = mvzr.compile("\\d+").?;
+            var numIter = numRegex.iterator(match.slice);
+            var mulAcc: u32 = 1;
+            while (numIter.next()) |num| mulAcc *= try std.fmt.parseInt(u32, num.slice, 10);
+            acc += mulAcc;
+        }
+    }
+    return acc;
+}
+fn part1() !void {
+    std.debug.print("part1: {d}\n", .{try doThing(false)});
+}
+
+fn part2() !void {
+    std.debug.print("part2: {d}\n", .{try doThing(true)});
+}
+
+pub export fn day03() void {
+    std.debug.print("-day03-\n", .{});
+    part1() catch unreachable;
+    part2() catch unreachable;
+}
--- src/main.zig
+++ src/main.zig
@@ -2,8 +2,10 @@ const std = @import("std");
 
 const day01 = @import("./day01.zig").day01;
 const day02 = @import("./day02.zig").day02;
+const day03 = @import("./day03.zig").day03;
 
 pub fn main() void {
     day01();
     day02();
+    day03();
 }
