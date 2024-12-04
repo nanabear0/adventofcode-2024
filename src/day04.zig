46e5adb673372baa7bb1b5bc363085fa482691fa
Denizhan Erdem <denizhanerdem@gmail.com>
2024-12-04 23:52:59 +0000

    day4 refacror

--- src/day04.zig
+++ src/day04.zig
@@ -1,9 +1,17 @@
 const std = @import("std");
-const mvzr = @import("mvzr");
 
 const input = std.mem.trim(u8, @embedFile("inputs/day04.txt"), "\n");
 
-const Point = struct { x: isize, y: isize };
+const Point = struct {
+    x: isize,
+    y: isize,
+    fn add(self: *Point, other: Point) Point {
+        return Point{ .x = self.x + other.x, .y = self.y + other.y };
+    }
+    fn subtract(self: *Point, other: Point) Point {
+        return Point{ .x = self.x - other.x, .y = self.y - other.y };
+    }
+};
 
 const directions = [8]Point{
     Point{ .x = 0, .y = 1 },
@@ -15,14 +23,16 @@ const directions = [8]Point{
     Point{ .x = -1, .y = 0 },
     Point{ .x = -1, .y = -1 },
 };
+
 const xmas = "XMAS";
 
 fn part1() !void {
-    var lineIter = std.mem.split(u8, input, "\n");
     var map = std.AutoHashMap(Point, u8).init(std.heap.page_allocator);
     defer map.deinit();
     var xs = std.AutoHashMap(Point, void).init(std.heap.page_allocator);
     defer xs.deinit();
+
+    var lineIter = std.mem.split(u8, input, "\n");
     var y: isize = 0;
     while (lineIter.next()) |line| {
         defer y += 1;
@@ -43,8 +53,8 @@ fn part1() !void {
         directions: for (directions) |dir| {
             var pointToCheck = Point{ .x = xpos.x, .y = xpos.y };
             for (xmas) |expectedChar| {
-                defer pointToCheck = Point{ .x = pointToCheck.x + dir.x, .y = pointToCheck.y + dir.y };
-                if ((map.get(pointToCheck) orelse '.') != expectedChar) continue :directions;
+                defer pointToCheck = pointToCheck.add(dir);
+                if (map.get(pointToCheck) != expectedChar) continue :directions;
             }
             xmasCount += 1;
         }
@@ -59,12 +69,14 @@ const diagonalDirections = [4]Point{
     Point{ .x = -1, .y = 1 },
     Point{ .x = -1, .y = -1 },
 };
+
 fn part2() !void {
-    var lineIter = std.mem.split(u8, input, "\n");
     var map = std.AutoHashMap(Point, u8).init(std.heap.page_allocator);
     defer map.deinit();
     var as = std.AutoHashMap(Point, void).init(std.heap.page_allocator);
     defer as.deinit();
+
+    var lineIter = std.mem.split(u8, input, "\n");
     var y: isize = 0;
     while (lineIter.next()) |line| {
         defer y += 1;
@@ -84,10 +96,10 @@ fn part2() !void {
     while (asIter.next()) |apos| {
         var masCount: usize = 0;
         for (diagonalDirections) |dir| {
-            const previous = Point{ .x = apos.x - dir.x, .y = apos.y - dir.y };
-            const next = Point{ .x = apos.x + dir.x, .y = apos.y + dir.y };
+            const previous = apos.subtract(dir);
+            const next = apos.add(dir);
 
-            if ((map.get(previous) orelse '.') == 'M' and (map.get(next) orelse '.') == 'S') masCount += 1;
+            if (map.get(previous) == 'M' and map.get(next) == 'S') masCount += 1;
         }
 
         if (masCount == 2) x_masCount += 1;
--- src/main.zig
+++ src/main.zig
@@ -6,8 +6,8 @@ const day03 = @import("./day03.zig").day03;
 const day04 = @import("./day04.zig").day04;
 
 pub fn main() void {
-    // day01();
-    // day02();
-    // day03();
+    day01();
+    day02();
+    day03();
     day04();
 }
