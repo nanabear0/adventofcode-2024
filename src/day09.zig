55f5c8cca3ddcc4e46c8233788edd077cb14c0f4
Denizhan Erdem <derdem@microsoft.com>
2024-12-09 18:02:16 +0000

    useless backup

--- src/day09.zig
+++ src/day09.zig
@@ -45,8 +45,66 @@ fn part1() !void {
     std.debug.print("part1: {any}\n", .{result});
 }
 
+fn part2() !void {
+    var blocks = std.ArrayList(?usize).init(gpa);
+    defer blocks.deinit();
+
+    var id: usize = 0;
+    var isFile = true;
+    for (input) |char| {
+        const num: usize = @intCast(char - 48);
+        if (isFile) {
+            try blocks.appendNTimes(id, num);
+            id += 1;
+            isFile = false;
+        } else {
+            try blocks.appendNTimes(null, num);
+            isFile = true;
+        }
+    }
+
+    var workingWithSlice = blocks.items[0..];
+    var poop: usize = 0;
+    while (workingWithSlice.len > 0) {
+        poop += 1;
+        std.debug.print("------{any}------\n", .{workingWithSlice});
+        if (std.mem.indexOf(?usize, workingWithSlice, &[_]?usize{null})) |emptySpotStart| {
+            if (std.mem.indexOfNone(?usize, workingWithSlice[emptySpotStart..], &[_]?usize{null})) |emptySpotSize| {
+                var targetSearchSlice = workingWithSlice[emptySpotStart + emptySpotSize ..];
+                while (targetSearchSlice.len > 0) {
+                    if (std.mem.indexOf(?usize, targetSearchSlice, &[_]?usize{targetSearchSlice[targetSearchSlice.len - 1]})) |startOfReplacementSlice| {
+                        const sizeOfSlice = targetSearchSlice.len - startOfReplacementSlice;
+                        std.debug.print("{any}, {any},{d},{d}\n", .{ targetSearchSlice, targetSearchSlice[startOfReplacementSlice..], emptySpotSize, sizeOfSlice });
+                        if (emptySpotSize >= sizeOfSlice) {
+                            for (0..sizeOfSlice) |i| {
+                                workingWithSlice[emptySpotStart + i] = targetSearchSlice[targetSearchSlice.len - 1];
+                                targetSearchSlice[startOfReplacementSlice + i] = null;
+                            }
+                            workingWithSlice = workingWithSlice[emptySpotStart + sizeOfSlice .. workingWithSlice.len - 1 - sizeOfSlice];
+                            break;
+                        } else {
+                            if (std.mem.lastIndexOfNone(?usize, targetSearchSlice, &[_]?usize{ targetSearchSlice[targetSearchSlice.len - 1], null })) |startOfNextFile| {
+                                targetSearchSlice = targetSearchSlice[0 .. startOfNextFile + 1];
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+                }
+                // std.mem.lastIndexOf(?usize, haystack: workingWithSlice[emptySpotSize+])
+                // break;
+                // workingWithSlice = std.mem.trimRight(?usize, workingWithSlice[emptySpotStart + emptySpotSize ..], &[_]?usize{null});
+            } else break;
+        } else break;
+        if (poop > 20) break;
+    }
+
+    std.debug.print("part2: {any}\n", .{0});
+}
+
 pub export fn day09() void {
     std.debug.print("-day09-\n", .{});
 
-    part1() catch unreachable;
+    // part1() catch unreachable;
+    part2() catch unreachable;
 }
