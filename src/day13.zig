8e77adccde64edc50dd739cbb178e03603abf303
Denizhan Erdem <denizhanerdem@gmail.com>
2024-12-13 12:47:31 +0000

    all my homies hate floats

--- src/day13.zig
+++ src/day13.zig
@@ -7,33 +7,36 @@ const gpa = gpa_impl.allocator();
 
 const input = std.mem.trim(u8, @embedFile("inputs/day13.txt"), "\n");
 
-fn doThing(bigboi: bool) !usize {
+fn doThing(bigboi: bool) !isize {
     const numRegex = mvzr.compile("[0-9]+").?;
     var numIter = numRegex.iterator(input);
-    var tokens: usize = 0;
-    var n: usize = 0;
+    var tokens: isize = 0;
+    var n: isize = 0;
     while (numIter.next()) |amatch| {
         n += 1;
-        const a = try std.fmt.parseFloat(f128, amatch.slice);
-        const d = try std.fmt.parseFloat(f128, numIter.next().?.slice);
-        const b = try std.fmt.parseFloat(f128, numIter.next().?.slice);
-        const e = try std.fmt.parseFloat(f128, numIter.next().?.slice);
-        var c = try std.fmt.parseFloat(f128, numIter.next().?.slice);
-        var f = try std.fmt.parseFloat(f128, numIter.next().?.slice);
+        const a = try std.fmt.parseInt(isize, amatch.slice, 10);
+        const d = try std.fmt.parseInt(isize, numIter.next().?.slice, 10);
+        const b = try std.fmt.parseInt(isize, numIter.next().?.slice, 10);
+        const e = try std.fmt.parseInt(isize, numIter.next().?.slice, 10);
+        var c = try std.fmt.parseInt(isize, numIter.next().?.slice, 10);
+        var f = try std.fmt.parseInt(isize, numIter.next().?.slice, 10);
         if (bigboi) {
             c += 10000000000000;
             f += 10000000000000;
         }
 
-        const y: f128 = (c / a - f / d) / (b / a - e / d);
-        const x: f128 = (c - b * y) / a;
-        const roundy = @round(y);
-        const roundx = @round(x);
-        if (x >= 0 and y >= 0 and
-            std.math.approxEqAbs(f128, x, roundx, 0.0000001) and
-            std.math.approxEqAbs(f128, y, roundy, 0.0000001))
-        {
-            tokens += @intFromFloat(roundx * 3 + roundy);
+        const ybig = f * a - c * d;
+        const ysmol = a * e - b * d;
+        if (@mod(ybig, ysmol) != 0) continue;
+        const y = @divExact(ybig, ysmol);
+
+        const xbig = c - b * y;
+        const xsmol = a;
+        if (@mod(xbig, xsmol) != 0) continue;
+        const x = @divExact(xbig, xsmol);
+
+        if (x >= 0 and y >= 0) {
+            tokens += x * 3 + y;
         }
     }
     return tokens;
