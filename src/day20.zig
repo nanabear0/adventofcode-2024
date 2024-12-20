4ebe07270fa73b8d1874ab54d56fd5d99b5ff9d1
Denizhan Erdem <derdem@microsoft.com>
2024-12-20 15:32:00 +0000

    poopformance 20-1

--- src/day20.zig
+++ src/day20.zig
@@ -8,58 +8,14 @@ const gpa = gpa_impl.allocator();
 
 const input = std.mem.trim(u8, @embedFile("inputs/day20.txt"), "\n");
 
-const PossibleCheats = [_][2]Point{
-    [2]Point{ Point{ .x = 0, .y = -1 }, Point{ .x = 0, .y = -1 } },
-    [2]Point{ Point{ .x = 0, .y = -1 }, Point{ .x = 1, .y = 0 } },
-    [2]Point{ Point{ .x = 0, .y = -1 }, Point{ .x = -1, .y = 0 } },
+const CheatsInvolvingBlockage = [_][2]Point{
+    [2]Point{ CardinalDirections[0], CardinalDirections[2] },
+    [2]Point{ CardinalDirections[1], CardinalDirections[3] },
 
-    [2]Point{ Point{ .x = 1, .y = 0 }, Point{ .x = 1, .y = 0 } },
-    [2]Point{ Point{ .x = 1, .y = 0 }, Point{ .x = 0, .y = -1 } },
-    [2]Point{ Point{ .x = 1, .y = 0 }, Point{ .x = 0, .y = 1 } },
-
-    [2]Point{ Point{ .x = 0, .y = 1 }, Point{ .x = 0, .y = 1 } },
-    [2]Point{ Point{ .x = 0, .y = 1 }, Point{ .x = -1, .y = 0 } },
-    [2]Point{ Point{ .x = 0, .y = 1 }, Point{ .x = 1, .y = 0 } },
-
-    [2]Point{ Point{ .x = -1, .y = 0 }, Point{ .x = -1, .y = 0 } },
-    [2]Point{ Point{ .x = -1, .y = 0 }, Point{ .x = 0, .y = -1 } },
-    [2]Point{ Point{ .x = -1, .y = 0 }, Point{ .x = 0, .y = 1 } },
-};
-
-const MovementCost = struct {
-    cheatable: Cheatables,
-    cost: isize,
-    pub fn canCheat(self: *const MovementCost) bool {
-        return self.cheatable.canCheat();
-    }
-    pub fn move(self: *const MovementCost, dir: Point) MovementCost {
-        return MovementCost{ .cheatable = self.cheatable.move(dir), .cost = self.cost + 1 };
-    }
-    pub fn cheat(self: *const MovementCost, dir1: Point, dir2: Point) MovementCost {
-        return MovementCost{ .cheatable = self.cheatable.cheat(dir1, dir2), .cost = self.cost + 2 };
-    }
-    pub fn comp(_: void, a: MovementCost, b: MovementCost) std.math.Order {
-        return std.math.order(a.cost, b.cost);
-    }
-};
-
-const Cheatables = struct {
-    point: Point,
-    usedCheats: ?[2]Point,
-    pub fn canCheat(self: *const Cheatables) bool {
-        return self.usedCheats == null;
-    }
-    pub fn move(self: *const Cheatables, dir: Point) Cheatables {
-        return Cheatables{ .point = self.point.add(dir), .usedCheats = self.usedCheats };
-    }
-    pub fn cheat(self: *const Cheatables, dir1: Point, dir2: Point) Cheatables {
-        return Cheatables{ .point = self.point.add(dir1).add(dir2), .usedCheats = .{ self.point.add(dir1), self.point.add(dir1).add(dir2) } };
-    }
-};
-
-const CostOfCheating = struct {
-    cost: isize,
-    hasCheated: false,
+    [2]Point{ CardinalDirections[0], CardinalDirections[1] },
+    [2]Point{ CardinalDirections[1], CardinalDirections[2] },
+    [2]Point{ CardinalDirections[2], CardinalDirections[3] },
+    [2]Point{ CardinalDirections[3], CardinalDirections[0] },
 };
 
 const MapEntry = enum {
@@ -67,11 +23,50 @@ const MapEntry = enum {
     Blocked,
 };
 
+const MovementCost = struct {
+    point: Point,
+    cost: isize,
+    pub fn move(self: *const MovementCost, dir: Point) MovementCost {
+        return MovementCost{ .point = self.point.add(dir), .cost = self.cost + 1 };
+    }
+    pub fn comp(_: void, a: MovementCost, b: MovementCost) std.math.Order {
+        return std.math.order(a.cost, b.cost);
+    }
+};
+
+fn findShortestPathBetweenTwoPoints(start: Point, end: Point, map: *std.AutoHashMap(Point, MapEntry)) !isize {
+    var visited = std.AutoHashMap(Point, isize).init(gpa);
+    var frontier = std.PriorityQueue(MovementCost, void, MovementCost.comp).init(gpa, {});
+    var bestPathEndings = std.AutoHashMap(MovementCost, void).init(gpa);
+    defer visited.deinit();
+    defer frontier.deinit();
+    defer bestPathEndings.deinit();
+
+    try visited.put(start, 0);
+    try frontier.add(MovementCost{ .point = start, .cost = 0 });
+
+    return while (frontier.removeOrNull()) |*current| {
+        if (current.point.equals(end)) {
+            break current.cost;
+        }
+        for (CardinalDirections) |dir| {
+            const option = current.move(dir);
+            if (map.get(option.point) != .Free) continue;
+            if ((visited.get(option.point) orelse std.math.maxInt(isize)) < option.cost) continue;
+
+            try frontier.add(option);
+            try visited.put(option.point, option.cost);
+        }
+    } else -1;
+}
+
 fn part1() !void {
+    var blockages = std.AutoHashMap(Point, void).init(gpa);
     var map = std.AutoHashMap(Point, MapEntry).init(gpa);
     var start: Point = Point{ .x = 0, .y = 0 };
     var end: Point = Point{ .x = 0, .y = 0 };
     defer map.deinit();
+    defer blockages.deinit();
 
     var inputIter = std.mem.splitSequence(u8, input, "\n");
     var y: isize = 0;
@@ -80,7 +75,10 @@ fn part1() !void {
             const point = Point{ .x = @intCast(x), .y = y };
             switch (char) {
                 '.' => try map.put(point, .Free),
-                '#' => try map.put(point, .Blocked),
+                '#' => {
+                    try map.put(point, .Blocked);
+                    try blockages.put(point, {});
+                },
                 'S' => {
                     start = point;
                     try map.put(point, .Free);
@@ -94,52 +92,42 @@ fn part1() !void {
         }
     }
 
-    var visited = std.AutoHashMap(Point, isize).init(gpa);
-    var frontier = std.PriorityQueue(MovementCost, void, MovementCost.comp).init(gpa, {});
-    var cheatEndingCosts = std.AutoHashMap(?[2]Point, isize).init(gpa);
-    defer visited.deinit();
-    defer frontier.deinit();
-    defer cheatEndingCosts.deinit();
+    var shortestPathCache = std.AutoHashMap([2]Point, isize).init(gpa);
+    defer shortestPathCache.deinit();
+    var timeSavers = std.AutoHashMap(usize, usize).init(gpa);
+    defer timeSavers.deinit();
 
-    try visited.put(Cheatables{ .point = start, .usedCheats = null }, 0);
-    try frontier.add(MovementCost{ .cheatable = Cheatables{ .point = start, .usedCheats = null }, .cost = 0 });
+    var blockagesIter = blockages.keyIterator();
+    while (blockagesIter.next()) |blockage| {
+        for (CheatsInvolvingBlockage) |cheatDir| {
+            const cheatTerminals = [2]Point{ blockage.add(cheatDir[0]), blockage.add(cheatDir[1]) };
 
-    var bestPath: isize = std.math.maxInt(isize);
-    while (frontier.removeOrNull()) |*current| {
-        // std.debug.print("{d}\n", .{frontier.count()});
-        if (current.cheatable.point.equals(end)) {
-            // if (bestPath >= current.cost) {
-            try cheatEndingCosts.put(current.cheatable.usedCheats, current.cost);
-            bestPath = current.cost;
-            // }
-            continue;
-        }
+            if (!shortestPathCache.contains(cheatTerminals)) {
+                if (map.get(cheatTerminals[0]) != .Free or map.get(cheatTerminals[0]) != .Free) continue;
+                try shortestPathCache.put(cheatTerminals, try findShortestPathBetweenTwoPoints(cheatTerminals[0], cheatTerminals[1], &map));
+            }
 
-        var options = std.ArrayList(MovementCost).init(gpa);
-        for (CardinalDirections) |dir| {
-            try options.append(current.move(dir));
-        }
-        if (current.canCheat()) {
-            for (PossibleCheats) |cheat| {
-                try options.append(current.cheat(cheat[0], cheat[1]));
+            if (shortestPathCache.get(cheatTerminals)) |distance| {
+                // std.debug.print("shortest path between {any} and {any} is {d}\n", .{ cheatTerminals[0], cheatTerminals[1], distance });
+                const timeSave = distance - 2;
+                if (timeSave > 0) {
+                    const entry = try timeSavers.getOrPutValue(@intCast(timeSave), 0);
+                    entry.value_ptr.* += 1;
+                }
             }
         }
+    }
 
-        for (options.items) |option| {
-            if (option.cost > 9344) continue;
-            if (map.get(option.cheatable.point) != .Free) continue;
-            if ((visited.get(option.cheatable) orelse std.math.maxInt(isize)) < option.cost) continue;
-
-            try frontier.add(option);
-            try visited.put(option.cheatable, option.cost);
+    var result: usize = 0;
+    var timeSaversIter = timeSavers.iterator();
+    while (timeSaversIter.next()) |timeSave| {
+        if (timeSave.key_ptr.* >= 100) {
+            result += timeSave.value_ptr.*;
         }
-    }
-    var cheatEndingsIter = cheatEndingCosts.iterator();
-    while (cheatEndingsIter.next()) |entry| {
-        std.debug.print("{any} -> {any}\n", .{ entry.key_ptr.*, entry.value_ptr.* });
+        // std.debug.print("There are {d} cheats that save {d} picoseconds.\n", .{ timeSave.value_ptr.*, timeSave.key_ptr.* });
     }
 
-    std.debug.print("part1: {d}\n", .{0});
+    std.debug.print("part1: {d}\n", .{result});
 }
 
 fn part2() !void {
