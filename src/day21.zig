8a0e7a1cb2bbe499ff0ebb9ae497ae1a69aa1a53
Denizhan Erdem <denizhanerdem@gmail.com>
2024-12-21 17:05:17 +0000

    push poop

--- src/day21.zig
+++ src/day21.zig
@@ -36,41 +36,53 @@ fn fillInputGrid() !std.AutoArrayHashMap(u8, Point) {
     return inputGrid;
 }
 
-fn addShortestPath(start: u8, end: u8, grid: *const std.AutoArrayHashMap(u8, Point), path: *std.ArrayList(u8), _: bool) !void {
+fn addShortestPath(start: u8, end: u8, grid: *const std.AutoArrayHashMap(u8, Point), paths: *std.ArrayList(std.ArrayList(u8)), isNumber: bool) !void {
     if (grid.get(start)) |p1| {
         if (grid.get(end)) |p2| {
             const moveX = p2.x - p1.x;
             const moveY = p2.y - p1.y;
+            const pathEdge1 = Point{ .x = p1.x, .y = p2.y };
+            const pathEdge2 = Point{ .x = p2.x, .y = p1.y };
+            const invalidPoint = if (isNumber) Point{ .x = 0, .y = 3 } else Point{ .x = 0, .y = 0 };
             const absMoveX = @abs(moveX);
             const absMoveY = @abs(moveY);
-            if (moveX > 0) {
-                if (moveY > 0) {
-                    try path.appendNTimes('>', absMoveX);
-                    try path.appendNTimes('v', absMoveY);
+            const path1valid = !pathEdge1.equals(invalidPoint);
+            const path2valid = !pathEdge2.equals(invalidPoint);
+            const patshLen = paths.items.len;
+            for (0..patshLen) |i| {
+                var path = paths.items[i];
+                if (path1valid and path2valid) {
+                    try path.appendNTimes(if (moveX > 0) '>' else '<', absMoveX);
+                    try path.appendNTimes(if (moveY > 0) 'v' else '^', absMoveY);
+                    var clone = std.ArrayList(u8).init(gpa);
+                    for (path.items) |p| try clone.append(p);
+                    try clone.appendNTimes(if (moveY > 0) 'v' else '^', absMoveY);
+                    try clone.appendNTimes(if (moveX > 0) '>' else '<', absMoveX);
+                    try paths.append(clone);
                 } else {
-                    try path.appendNTimes('>', absMoveX);
-                    try path.appendNTimes('^', absMoveY);
-                }
-            } else {
-                if (moveY > 0) {
-                    try path.appendNTimes('v', absMoveY);
-                    try path.appendNTimes('<', absMoveX);
-                } else {
-                    try path.appendNTimes('^', absMoveY);
-                    try path.appendNTimes('<', absMoveX);
+                    if (moveX > 0) {
+                        try path.appendNTimes(if (moveX > 0) '>' else '<', absMoveX);
+                        try path.appendNTimes(if (moveY > 0) 'v' else '^', absMoveY);
+                    } else {
+                        try path.appendNTimes(if (moveY > 0) 'v' else '^', absMoveY);
+                        try path.appendNTimes(if (moveX > 0) '>' else '<', absMoveX);
+                    }
                 }
             }
         }
     }
-    try path.append('A');
-}
-fn getTotalPathForString(str: []const u8, grid: *const std.AutoArrayHashMap(u8, Point), isNumberGrid: bool) !std.ArrayList(u8) {
-    var path = std.ArrayList(u8).init(gpa);
-    try addShortestPath('A', str[0], grid, &path, isNumberGrid);
-    for (1..str.len) |i| {
-        try addShortestPath(str[i - 1], str[i], grid, &path, isNumberGrid);
+    for (paths.items) |*path| {
+        try path.append('A');
     }
-    return path;
+}
+fn getTotalPathForString(str: []const u8, grid: *const std.AutoArrayHashMap(u8, Point), isNumberGrid: bool) !std.ArrayList(std.ArrayList(u8)) {
+    var paths = std.ArrayList(std.ArrayList(u8)).init(gpa);
+    try paths.append(std.ArrayList(u8).init(gpa));
+    try addShortestPath('A', str[0], grid, &paths, isNumberGrid);
+    for (1..str.len) |i| {
+        try addShortestPath(str[i - 1], str[i], grid, &paths, isNumberGrid);
+    }
+    return paths;
 }
 
 fn doThing() !void {
@@ -81,16 +93,23 @@ fn doThing() !void {
 
     var inputIter = std.mem.splitScalar(u8, input, '\n');
     while (inputIter.next()) |line| {
-        const level0Path = try getTotalPathForString(line, &numberGrid, true);
-        defer level0Path.deinit();
-        const level1Path = try getTotalPathForString(level0Path.items, &inputGrid, false);
-        defer level1Path.deinit();
-        const level2Path = try getTotalPathForString(level1Path.items, &inputGrid, false);
-        defer level2Path.deinit();
+        const level0Paths = try getTotalPathForString(line, &numberGrid, true);
+        var level1Paths = std.ArrayList(std.ArrayList(u8)).init(gpa);
+        for (level0Paths.items) |level0Path| {
+            const tmp = try getTotalPathForString(level0Path.items, &inputGrid, false);
+            // _ = tmp;
+            try level1Paths.appendSlice(tmp.items[0..]);
+        }
+        var level2Paths = std.ArrayList(std.ArrayList(u8)).init(gpa);
+        for (level1Paths.items) |level1Path| {
+            const tmp = try getTotalPathForString(level1Path.items, &inputGrid, false);
+            // _ = tmp;
+            try level2Paths.appendSlice(tmp.items[0..]);
+        }
         std.debug.print("\n==={s}===\n", .{line});
-        std.debug.print("{d} {s}\n", .{ level0Path.items.len, level0Path.items });
-        std.debug.print("{d} {s}\n", .{ level1Path.items.len, level1Path.items });
-        std.debug.print("{d} {s}\n", .{ level2Path.items.len, level2Path.items });
+        std.debug.print("{d} {s}\n", .{ level0Paths.items.len, level0Paths.items[0].items });
+        std.debug.print("{d} {s}\n", .{ level1Paths.items.len, level1Paths.items[0].items });
+        std.debug.print("{d} {s}\n", .{ level2Paths.items.len, level2Paths.items[0].items });
     }
 }
 
