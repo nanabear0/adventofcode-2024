f2bc1b8dc629233d4b117fa1530226b6795203ba
Denizhan Erdem <denizhanerdem@gmail.com>
2024-12-22 17:20:03 +0000

    only bitwise

--- src/day22.zig
+++ src/day22.zig
@@ -13,22 +13,17 @@ fn mix(a: usize, b: usize) usize {
 }
 
 fn prune(in: usize) usize {
-    return in % 16777216;
+    return in & 16777215;
 }
 
 fn generate(secret: usize) usize {
-    var result = prune(mix(secret * 64, secret));
-    result = prune(mix(result / 32, result));
-    result = prune(mix(result * 2048, result));
+    var result = prune(mix(secret << 6, secret));
+    result = prune(mix(result >> 5, result));
+    result = prune(mix(result << 11, result));
     return result;
 }
 
-fn recursiveGenerate(secret: usize, times: usize) usize {
-    if (times == 0) return secret;
-    return recursiveGenerate(generate(secret), times - 1);
-}
-
-fn part2() !void {
+fn doThing() !void {
     var inputIter = std.mem.splitScalar(u8, input, '\n');
     var saleResults = std.AutoHashMap([4]isize, std.ArrayList(isize)).init(gpa);
     var prices = try std.ArrayList(isize).initCapacity(gpa, 2001);
@@ -42,16 +37,19 @@ fn part2() !void {
     defer prices.deinit();
     defer changes.deinit();
     defer foundSales.deinit();
+
+    var p1Result: usize = 0;
     while (inputIter.next()) |line| {
         prices.clearRetainingCapacity();
         changes.clearRetainingCapacity();
         foundSales.clearRetainingCapacity();
         var secret: usize = try std.fmt.parseInt(usize, line, 10);
-        for (0..2001) |_| {
-            try prices.append(@intCast(secret % 10));
+        try prices.append(@intCast(secret % 10));
+        for (0..2000) |_| {
             secret = generate(secret);
+            try prices.append(@intCast(secret % 10));
         }
-
+        p1Result += secret;
         try changes.append(prices.items[0]);
         for (1..prices.items.len) |i| {
             try changes.append(prices.items[i] - prices.items[i - 1]);
@@ -70,28 +68,17 @@ fn part2() !void {
             try saleResults.getPtr(seq).?.append(prices.items[i]);
         }
     }
-    var banyanyas: isize = 0;
+    var p2Result: isize = 0;
     var saleResultsIter = saleResults.iterator();
     while (saleResultsIter.next()) |sales| {
         var sum: isize = 0;
         for (sales.value_ptr.items) |sale| sum += sale;
-        if (banyanyas < sum) banyanyas = sum;
+        if (p2Result < sum) p2Result = sum;
     }
-    std.debug.print("part2: {d}\n", .{banyanyas});
-}
-
-fn part1() !void {
-    var inputIter = std.mem.splitScalar(u8, input, '\n');
-    var result: usize = 0;
-    while (inputIter.next()) |line| {
-        const secret: usize = try std.fmt.parseInt(usize, line, 10);
-        const out = recursiveGenerate(secret, 2000);
-        result += out;
-    }
-    std.debug.print("part1: {d}\n", .{result});
+    std.debug.print("part1: {d}\n", .{p1Result});
+    std.debug.print("part2: {d}\n", .{p2Result});
 }
 
 pub export fn day22() void {
-    part1() catch unreachable;
-    part2() catch unreachable;
+    doThing() catch unreachable;
 }
